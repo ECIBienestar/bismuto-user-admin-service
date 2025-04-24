800e6d81cf35d3734d47032c8be3000c90df0190
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2025-04-24 03:28:56 -0500

    New Files by Jesus Pinzon

---
+++ src/main/java/edu/eci/cvds/users/UserAdminApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.users;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserAdminApplication {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
